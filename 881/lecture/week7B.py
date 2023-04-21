92a7aee2d0c132a67764fbb9ca862cc30d03549e
chenkan0617 <chenkan0617@outlook.com>
2023-04-21 16:20:56 +1000

    week7b 881

--- 881/lecture/week7B.py
+++ 881/lecture/week7B.py
@@ -1,5 +1,18 @@
 import numpy
 
-arr = numpy.array([1,2,3])
+# arr = numpy.array(['frog', 'cat', 'dog'])
+
+# print(arr)
+
+# arr = numpy.zeros(shape=10, dtype=numpy.int32)
+
+# print(arr)
+
+
+# arr = numpy.ones(shape=10, dtype=bool)
+
+# arr = numpy.array([[8, 1, 6], [3, 5, 7], [4, 9, 2]])
+
+arr = numpy.zeros(shape=(3, 4), dtype=numpy.int64)
 
 print(arr)
