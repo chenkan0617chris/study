654ea53608370b23da67aeedf1be7a264ad76524
chenkan0617 <chenkan0617@outlook.com>
2024-03-14 00:11:56 +1100

    1

--- 9307/lab/week3/task2.php
+++ 9307/lab/week3/task2.php
@@ -10,9 +10,11 @@
     <?php
         function checkISBN(string $ISBN) {
             $pattern = "/ISBN' ' [0-9]{3}-[0-9]-[0-9]{2}-[0-9]{6}-[0-9]/";
+
+            return preg_match($pattern, $ISBN);
         }
 
-        
+        echo checkISBN('ISBN 999-9-99-999999-9');
     ?>
 </body>
 </html>
