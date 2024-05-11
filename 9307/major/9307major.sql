2547336fcfcff3893bd200bc49d3e7c7dab68231
chenkan0617 <chenkan0617@outlook.com>
2024-05-11 14:56:41 +1000

    sql

--- 9307/major/9307major.sql
+++ 9307/major/9307major.sql
@@ -1,7 +1,6 @@
 create database easyparking;
 use easyparking;
 
-
 create table users (
 	id int auto_increment primary key,
     username char(30) unique not null,
@@ -33,4 +32,13 @@ create table parkings (
     foreign key (username) references users(username),
     foreign key (location_id) references locations(id)
 );
+
+insert into users (username, password, name, surname, phone, email, type) values ('admin1', md5('asdasd'), 'admin', 'aaa', '0456345234', 'test@admin.com', 'administrator');
+insert into users (username, password, name, surname, phone, email, type) values ('user1', md5('asdasd'), 'user', 'bbb', '0456345234', 'test@user.com', 'user');
+
+insert into locations values ('P1', '30 Rowland Avenue', 'this is a parking', 20, 10);
+insert into locations values ('P2', '62 Jobson Avenue', 'this is a test description', 40, 5);
+
+insert into parkings (username, location_id, start_time) value ('user1', 'P1', '2024-05-09 20:38:50');
+insert into parkings (username, location_id, start_time) value ('user1', 'P2', '2024-05-10 00:02:47');
 
